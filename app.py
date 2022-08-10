79f71a71dcaadbecf1994f162ecc67db7045149f
Fernando Vergara <fernandovergara@MacBook-Air-de-Fernando-Vergara.local>
2022-08-09 21:58:24 -0500

    app.py

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ mask = (df['date'] > '2020/1/1') & (df['date'] <= '2020/6/30')
 df1=df.loc[mask]
 df3_6meses=df1[['date','state','total_adult_patients_hospitalized_confirmed_covid','total_pediatric_patients_hospitalized_confirmed_covid']].copy()
 df3_6meses.reset_index(inplace=True, drop=True)
-df3_6meses.rename(columns={'total_adult_patients_hospitalized_confirmed_covid':'total_adult','total_pediatric_patients_hospitalized_confirmed_covid':'total_pediatric'}, inplace=True)
+df3_6meses.rename(columns={'total_adult_patients_hospitalized_confirmed_covid':'Total Adult','total_pediatric_patients_hospitalized_confirmed_covid':'Total Pediatric'}, inplace=True)
 df3_6meses= df3_6meses.fillna(0, axis=1)
-df3_6meses['total']=df3_6meses.total_adult+df3_6meses.total_pediatric
+df3_6meses['Total']=df3_6meses.total_adult+df3_6meses.total_pediatric
 top5_3 = df3_6meses.groupby('state').sum()
-top5_3 = top5_3.sort_values(by=['total'],inplace=False, ascending=False)
+top5_3 = top5_3.sort_values(by=['Total'],inplace=False, ascending=False)
 st.dataframe(data=top5_3, width=None, height=None)
